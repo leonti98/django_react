35740fa472583b730ba0ce4f5ff1962f9f209719
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-12-11 18:08:08 +0400

    Added funtion to fetch notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,40 @@
+import { useState, useEffect } from 'react';
+import api from '../api';
+
 const Home = () => {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    gethNotes();
+  }, []);
+
+  const gethNotes = async () => {
+    api
+      .get('/api/notes/')
+      .then((response) => response.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const deleteNote = async (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert('Note deleted');
+        } else {
+          console.error('Error');
+        }
+        gethNotes();
+      })
+      .catch((error) => console.error(error));
+  };
+
   return <div>Home</div>;
 };
 
