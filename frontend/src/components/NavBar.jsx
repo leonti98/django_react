62a4320e8ea39d2f9a6058d8be251c4549a9dd96
odin <git config --global user.email 123456789+odin@users.noreply.github.com>
2024-12-13 05:19:09 +0400

    Added link to navbar username

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 function MyNavBar() {
   return (
@@ -10,13 +11,13 @@ function MyNavBar() {
           <Navbar.Brand href="/">Blog</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              Signed in as:{' '}
-              <a href="#login">{localStorage.getItem('username')}</a>
+              Signed in as:
+              <Link to={`/user/${localStorage.getItem('user_id')}`}>
+                {localStorage.getItem('username')}
+              </Link>
             </Navbar.Text>
             <Nav.Link href="/logout" className="ms-3 text-warning">
               Logout
